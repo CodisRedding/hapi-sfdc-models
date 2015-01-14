31ea67a8bcc4f8de2f5b1a4cbc018f1ff3d507f9
Rocky Assad <rassad@gbhem.org>
2015-01-14 15:21:15 -0600

    reset free dev box

--- test/config.js
+++ test/config.js
@@ -3,7 +3,7 @@ module.exports = {
         url: 'https://login.salesforce.com',
         auth: {
 			user: 'salesforce.otr@gmail.com',
-			pass: 'dataervices@123456ChAiNbtuE0mFc1oc9E8BfEcl'
+			pass: 'sfdc12345w6YIUwOWrRSnob4nBdDd4Lmma'
 		}
     }
 };
