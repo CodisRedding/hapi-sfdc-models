a07a213e085280ea919303d04fdca1d6b546f567
Rocky Assad <rassad@gbhem.org>
2015-01-14 15:17:10 -0600

    add free devbox creds

--- test/config.js
+++ test/config.js
@@ -2,8 +2,8 @@ module.exports = {
     salesforce: {
         url: 'https://login.salesforce.com',
         auth: {
-			user: '<user>',
-			pass: '<pass><token>'
+			user: 'salesforce.otr@gmail.com',
+			pass: 'dataervices@123456ChAiNbtuE0mFc1oc9E8BfEcl'
 		}
     }
 };
